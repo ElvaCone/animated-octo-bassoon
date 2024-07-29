c66d5e0841c2bbed7ccf7ef63d7c81215f5c5b2b
ElvaCone <173384004+ElvaCone@users.noreply.github.com>
2024-07-29 04:51:31 +0300

    07---时钟案例

--- nodejs-learn/第一天/07---时钟案例.js
+++ nodejs-learn/第一天/07---时钟案例.js
@@ -6,7 +6,9 @@ const regScript = /<script>[\s\S]*<\/script>/
 
 fs.readFile(path.join(__dirname, './files/clock/index-素材.html'), 'utf-8', (err, dataStr) => {
     if (err) return console.log('读取素材失败！' + err.message);
-
+    resolveCSS(dataStr)
+    resolveJS(dataStr)
+    resolveHTML(dataStr)
 })
 
 function resolveCSS(dataStr) {
@@ -15,5 +17,22 @@ function resolveCSS(dataStr) {
     fs.writeFile(path.join(__dirname, './files/clock/index.css'), newCSS, 'utf-8', (err) => {
         if (err) return console.log('写入CSS失败！' + err.message);
         console.log('写入CSS成功！');
+    })
+}
+
+function resolveJS(dataStr) {
+    const arr = regScript.exec(dataStr)
+    const newJS = arr[0].replace('<script>', '').replace('</script>', '')
+    fs.writeFile(path.join(__dirname, './files/clock/index.js'), newJS, 'utf-8', (err) => {
+        if (err) return console.log('写入JS失败！' + err.message);
+        console.log('写入JS成功！');
+    })
+}
+
+function resolveHTML(dataStr) {
+    const newHTML = dataStr.replace(regStyle, '<link rel="stylesheet" href="./files/clock/index.css">').replace(regScript, '<script src="./files/clock/index.js"></script>')
+    fs.writeFile(path.join(__dirname, './files/clock/index.html'), newHTML, 'utf-8', (err) => {
+        if (err) return console.log('写入HTML失败！' + err.message);
+        console.log('写入HTML成功！');
     })
 }
