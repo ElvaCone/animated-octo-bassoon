a269c40ad6ea81b3bd0faa2a73cc609f35e73700
ElvaCone <173384004+ElvaCone@users.noreply.github.com>
2024-08-02 05:56:47 +0300

    increment

--- react-learn/第一天/react-redux/src/App.js
+++ react-learn/第一天/react-redux/src/App.js
@@ -1,10 +1,14 @@
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { increment, decrement } from './store/modules/counterStore'
 
 function App() {
   const { count } = useSelector(state => state.counter)
+  const dispatch = useDispatch()
   return (
     <div className="App">
+      <button onClick={() => dispatch(decrement())}>-</button>
       {count}
+      <button onClick={() => dispatch(increment())}>+</button>
     </div>
   );
 }
