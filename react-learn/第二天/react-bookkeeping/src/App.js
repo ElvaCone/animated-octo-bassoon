aa0a6b76a1ca04677ab1034b64cd0756e399454f
ElvaCone <173384004+ElvaCone@users.noreply.github.com>
2024-08-02 20:20:12 +0300

    craco

--- react-learn/第二天/react-bookkeeping/src/App.js
+++ react-learn/第二天/react-bookkeeping/src/App.js
@@ -1,8 +1,11 @@
+import sum from '@/testCraco' // craco.config.js起效了,@符号等同于src了 jsconfig.json起效了，有路径联想功能了
 
+console.log(sum(1, 3));
 
 function App() {
   return (
     <div className="App">
+      This is App.
     </div>
   );
 }
