04a2035d3902ba0ba018d8cdaa115068030b0dc6
ElvaCone <173384004+ElvaCone@users.noreply.github.com>
2024-08-03 10:22:11 +0300

    router

--- react-learn/第二天/react-bookkeeping/src/App.js
+++ react-learn/第二天/react-bookkeeping/src/App.js
@@ -1,12 +1,12 @@
 import sum from '@/testCraco' // craco.config.js起效了,@符号等同于src目录了 jsconfig.json起效了，有路径联想功能了
+import router from './router';
+import { RouterProvider } from 'react-router-dom'
 
 console.log(sum(1, 3));
 
 function App() {
   return (
-    <div className="App">
-      This is App.
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
