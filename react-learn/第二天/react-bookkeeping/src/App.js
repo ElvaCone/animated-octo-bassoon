c151e1b90fccab9285180de7f4ff585fea17653e
ElvaCone <173384004+ElvaCone@users.noreply.github.com>
2024-08-03 09:42:56 +0300

    0

--- react-learn/第二天/react-bookkeeping/src/App.js
+++ react-learn/第二天/react-bookkeeping/src/App.js
@@ -1,4 +1,4 @@
-import sum from '@/testCraco' // craco.config.js起效了,@符号等同于src了 jsconfig.json起效了，有路径联想功能了
+import sum from '@/testCraco' // craco.config.js起效了,@符号等同于src目录了 jsconfig.json起效了，有路径联想功能了
 
 console.log(sum(1, 3));
 
