581297a604d5dec2a62b2b24e94b87d63d36a883
ElvaCone <173384004+ElvaCone@users.noreply.github.com>
2024-08-10 11:35:45 +0300

    craco

---
+++ react-learn/第三天/react-jike/src/App.js
@@ -0,0 +1,12 @@
+import { Button } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      This is App.
+      <Button variant='primary'>按钮</Button>
+    </div>
+  );
+}
+
+export default App;
