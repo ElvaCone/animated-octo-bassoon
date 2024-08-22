c535f5e715eca35274e18c8b99f723cd49e77f68
ElvaCone <173384004+ElvaCone@users.noreply.github.com>
2024-08-22 11:00:25 +0300

    TestForwardRef

--- react-learn/第四天/first/src/components/TestForwardRef.js
+++ react-learn/第四天/first/src/components/TestForwardRef.js
@@ -5,18 +5,25 @@ function Son(props) {
     return <Form.Control type='text' {...props} />
 }
 
+function Son1({ inputRef, ...otherProps }) {
+    return <Form.Control type='text' ref={inputRef} {...otherProps} /> // 通过props传递ref
+}
+
 const ForwardRefSon = forwardRef((props, ref) => {
     return <Form.Control type='text' ref={ref} {...props} />
 })
 
 function TestForwardRef() {
     const sonRef = useRef(null)
+    const inputRef = useRef(null)
     const forwardRefSonRef = useRef(null)
 
     const showRef = () => {
         console.log(sonRef) // sonRef.current是null
+        console.log(inputRef)
         console.log(forwardRefSonRef)
         // sonRef.current.focus()
+        inputRef.current.focus()
         forwardRefSonRef.current.focus()
     }
 
@@ -27,6 +34,7 @@ function TestForwardRef() {
             </Button>
 
             <Son ref={sonRef} placeholder={'Son'} />
+            <Son1 inputRef={inputRef} placeholder={'Son1'} />
             <ForwardRefSon ref={forwardRefSonRef} placeholder={'ForwardRefSon'} />
         </Container>
     );
