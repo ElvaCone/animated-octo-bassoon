cbe7d81add1faf70854b71a57a9685ed094008eb
ElvaCone <173384004+ElvaCone@users.noreply.github.com>
2024-08-22 18:52:13 +0300

    zustand

--- react-learn/第四天/first/src/components/TestZustand.js
+++ react-learn/第四天/first/src/components/TestZustand.js
@@ -2,12 +2,15 @@ import { useEffect } from 'react';
 import { Button, Container, ListGroup } from 'react-bootstrap';
 import { create } from 'zustand'
 
-const useStore = create((set) => ({
+const createCounterSlice = (set) => ({
     count: 0,
     inc: () => set((state) => ({ count: state.count + 1 })), // 用到老数据
     setToNewCount: (newCount) => set({ count: newCount }), // 用到传进来的数据
     addNewCount: (newCount) => set((state) => ({ count: state.count + newCount })), // 既用到老数据又用到传进来的数据
     reset: () => set({ count: 0 }), // 什么都不用
+})
+
+const createChannelListSlice = (set) => ({
     channelList: [],
     fetchChannelList: async () => {
         const res = await fetch('http://geek.itheima.net/v1_0/channels')
@@ -15,12 +18,15 @@ const useStore = create((set) => ({
         console.log(jsonRes)
         set({ channelList: jsonRes.data.channels })
     }
+})
+
+const useBoundStore = create((...a) => ({
+    ...createCounterSlice(...a),
+    ...createChannelListSlice(...a)
 }))
 
-
-
 function TestZustand() {
-    const { count, inc, setToNewCount, reset, addNewCount, channelList, fetchChannelList } = useStore()
+    const { count, inc, setToNewCount, reset, addNewCount, channelList, fetchChannelList } = useBoundStore()
 
     useEffect(() => {
         fetchChannelList()
