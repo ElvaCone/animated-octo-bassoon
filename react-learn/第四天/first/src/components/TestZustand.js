0b1c022e7c010fa09b400249184b0684a41a709a
ElvaCone <173384004+ElvaCone@users.noreply.github.com>
2024-08-22 14:23:35 +0300

    TestZustand

--- react-learn/第四天/first/src/components/TestZustand.js
+++ react-learn/第四天/first/src/components/TestZustand.js
@@ -1,4 +1,5 @@
-import { Button, Container } from 'react-bootstrap';
+import { useEffect } from 'react';
+import { Button, Container, ListGroup } from 'react-bootstrap';
 import { create } from 'zustand'
 
 const useStore = create((set) => ({
@@ -7,12 +8,23 @@ const useStore = create((set) => ({
     setToNewCount: (newCount) => set({ count: newCount }), // 用到传进来的数据
     addNewCount: (newCount) => set((state) => ({ count: state.count + newCount })), // 既用到老数据又用到传进来的数据
     reset: () => set({ count: 0 }), // 什么都不用
+    channelList: [],
+    fetchChannelList: async () => {
+        const res = await fetch('http://geek.itheima.net/v1_0/channels')
+        const jsonRes = await res.json()
+        console.log(jsonRes)
+        set({ channelList: jsonRes.data.channels })
+    }
 }))
 
 
 
 function TestZustand() {
-    const { count, inc, setToNewCount, reset, addNewCount } = useStore()
+    const { count, inc, setToNewCount, reset, addNewCount, channelList, fetchChannelList } = useStore()
+
+    useEffect(() => {
+        fetchChannelList()
+    }, [fetchChannelList])
 
     return (
         <Container>
@@ -33,6 +45,14 @@ function TestZustand() {
             <Button variant="primary" onClick={reset}>
                 reset
             </Button>
+
+            <ListGroup>
+                {channelList.map((channel) => (
+                    <ListGroup.Item key={channel.id}>
+                        {channel.name}
+                    </ListGroup.Item>
+                ))}
+            </ListGroup>
         </Container>
     );
 }
