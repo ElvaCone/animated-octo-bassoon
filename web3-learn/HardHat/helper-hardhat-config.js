bd8de42e6356e5dc8a9fba73c14c11765365cb39
ElvaCone <173384004+ElvaCone@users.noreply.github.com>
2024-10-18 11:15:00 +0300

    1

--- web3-learn/HardHat/helper-hardhat-config.js
+++ web3-learn/HardHat/helper-hardhat-config.js
@@ -1,10 +1,10 @@
 
 module.exports = {
-    MockV3Aggregator_decimals: 8,
-    MockV3Aggregator_initialAnswer: 261312000000,
-    FundMe_lockTime: 180,
-    localChains: ['hardhat', 'local'],
-    testnetChainsConfig: {
+    MockV3Aggregator_decimals: 8, // 精度
+    MockV3Aggregator_initialAnswer: 263456000000, // 乘以10的8次方之后的结果
+    FundMe_lockTime: 180, // 单位是秒
+    localChains: ['hardhat', 'local'], // 本地网络
+    testnetChainsConfig: { // 测试网
         11155111: { // sepolia
             ethUsdDataFeedAddr: "0x694AA1769357215DE4FAC081bf1f309aDC325306"
         },
@@ -12,5 +12,5 @@ module.exports = {
             ethUsdDataFeed: "0x143db3CEEfbdfe5631aDD3E50f7614B6ba708BA7"
         }
     },
-    testnetWaitConfirmations: 2,
+    testnetWaitConfirmations: 2, // 等待几个块，防止etherscan还没收录
 }
--- web3-learn/HardHat/test/FundMe.test.js
+++ web3-learn/HardHat/test/FundMe.test.js
@@ -1,6 +1,7 @@
 const { ethers, deployments, getNamedAccounts } = require('hardhat');
 const { assert } = require('chai');
 const { FundMe_lockTime } = require('../helper-hardhat-config.js');
+const helpers = require('@nomicfoundation/hardhat-network-helpers');
 
 describe("test FundMe contract", async () => {
     let firstAccount
@@ -9,24 +10,24 @@ describe("test FundMe contract", async () => {
         await deployments.fixture(["all"])
         firstAccount = (await getNamedAccounts()).firstAccount
         const fundMeDeployment = await deployments.get("FundMe")
+        const mockV3AggregatorDeployment = await deployments.get("MockV3Aggregator")
         fundMe = await ethers.getContractAt("FundMe", fundMeDeployment.address)
     })
 
-    // it("test if the owner is msg.sender", async () => {
-    //     /* const [firstAccount] = await ethers.getSigners()
-    //     const fundMeFactory = await ethers.getContractFactory("FundMe")
-    //     const fundMe = await fundMeFactory.deploy(FundMe_lockTime) */
-    //     await fundMe.waitForDeployment()
-    //     assert.equal((await fundMe.owner()), firstAccount)
-    // })
+    it("owner is msg.sender", async () => {
+        /* const [firstAccount] = await ethers.getSigners()
+        const fundMeFactory = await ethers.getContractFactory("FundMe")
+        const fundMe = await fundMeFactory.deploy(FundMe_lockTime) */
+        assert.equal((await fundMe.owner()), firstAccount)
+    })
 
-    it("test if the dataFeed is assigned correctly", async () => {
-        assert.equal((await fundMe.dataFeed()), "0x694AA1769357215DE4FAC081bf1f309aDC325306") // strictEqual 会强制类型和值都相等，减少潜在的类型转换错误
+    it("dataFeed is assigned correctly", async () => {
+        assert.equal((await fundMe.dataFeed()), mockV3AggregatorDeployment.address) // strictEqual 会强制类型和值都相等，减少潜在的类型转换错误
     })
 
     // it("getFund", async () => {
-    //     const fundMeFactory = await ethers.getContractFactory("FundMe")
-    //     const fundMe = fundMeFactory.attach("0x246D249ac02A9E11e05AE92A00779BC251d92139")
+    //     /* const fundMeFactory = await ethers.getContractFactory("FundMe")
+    //     const fundMe = fundMeFactory.attach("0x246D249ac02A9E11e05AE92A00779BC251d92139") */
     //     await fundMe.getFund()
     // })
 })
