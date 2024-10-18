16972d051327d57223bc0222ccc914ba0e068a75
ElvaCone <173384004+ElvaCone@users.noreply.github.com>
2024-10-18 11:41:53 +0300

    1

--- web3-learn/HardHat/test/unit/FundMe.test.js
+++ web3-learn/HardHat/test/unit/FundMe.test.js
@@ -1,6 +1,6 @@
 const { ethers, deployments, getNamedAccounts } = require('hardhat');
-const { assert } = require('chai');
-const { FundMe_lockTime } = require('../helper-hardhat-config.js');
+const { assert, expect } = require('chai');
+const { FundMe_lockTime_more } = require('../../helper-hardhat-config.js');
 const helpers = require('@nomicfoundation/hardhat-network-helpers');
 
 describe("test FundMe contract", async () => {
@@ -18,11 +18,23 @@ describe("test FundMe contract", async () => {
         /* const [firstAccount] = await ethers.getSigners()
         const fundMeFactory = await ethers.getContractFactory("FundMe")
         const fundMe = await fundMeFactory.deploy(FundMe_lockTime) */
-        assert.equal((await fundMe.owner()), firstAccount)
+
+        // 必须加上 await，以确保所有异步操作已完成，再进行期望检查
+        await assert.equal((await fundMe.owner()), firstAccount)
     })
 
     it("dataFeed is assigned correctly", async () => {
-        assert.equal((await fundMe.dataFeed()), mockV3AggregatorDeployment.address) // strictEqual 会强制类型和值都相等，减少潜在的类型转换错误
+        await assert.equal((await fundMe.dataFeed()), mockV3AggregatorDeployment.address) // strictEqual 会强制类型和值都相等，减少潜在的类型转换错误
+    })
+
+    it("window closed, value enough, fund failed", async () => {
+        await helpers.time.increase(FundMe_lockTime_more)
+        await helpers.mine()
+        await expect(fundMe.fund({ value: ethers.parseEther("0.0003796") })).to.be.revertedWith("Window closed!")
+    })
+
+    it("window open, value not enough, fund failed", async () => {
+        await expect(fundMe.fund({ value: ethers.parseEther("0.0003795") })).to.be.revertedWith("Please send more!")
     })
 
     // it("getFund", async () => {
